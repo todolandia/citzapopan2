956f10389f99b280fff1c930cc38ba5116546d88
Citzapopan <citzapopan@gmail.com>
2026-03-16 16:56:49 -0600

    Revise fetch event handling in service worker
    Updated fetch event handler to first attempt network fetch and fallback to cache on failure.

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,11 @@
-const CACHE_NAME = "citzapopan-campus-v3";
-
-const urlsToCache = [
-"/campus.html"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then(response => {
-        return response || fetch(event.request);
+        return response;
       })
-  );
-});
-
-self.addEventListener("activate", event => {
-  const cacheWhitelist = [CACHE_NAME];
-
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
